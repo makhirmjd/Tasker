74b7ca7feb82889f152a1f0b55877561fc9248ed
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-07-31 21:20:03 +0100

    Creating the categories section 2

---
+++ Tasker/Models/MyTask.cs
@@ -0,0 +1,9 @@
+﻿namespace Tasker.Models;
+
+public class MyTask
+{
+    public string TaskName { get; set; } = default!;
+    public bool Completed { get; set; }
+    public int CategoryId { get; set; }
+    public string TaskColor { get; set; } = default!;
+}
--- Tasker/ViewModels/MainPageViewModel.cs
+++ Tasker/ViewModels/MainPageViewModel.cs
@@ -6,13 +6,16 @@ namespace Tasker.ViewModels;
 public class MainPageViewModel
 {
     public ObservableCollection<Category> Categories { get; set; } = [];
+    public ObservableCollection<MyTask> Tasks { get; set; } = [];
 
     public MainPageViewModel()
     {
         FillData();
     }
 
-    private void FillData() => Categories = [
+    private void FillData()
+    {
+        Categories = [
         new()
         {
                 Id = 1,
@@ -31,4 +34,66 @@ public class MainPageViewModel
                 CategoryName = "Shopping",
                 Color = "#14df80"
         }];
+
+        Tasks = [
+            new()
+            {
+                    TaskName = "Upload exercise files",
+                    Completed = false,
+                    CategoryId = 1
+            },
+            new()
+            {
+                    TaskName = "Plan next course",
+                    Completed = false,
+                    CategoryId = 1
+            },
+            new()
+            {
+                    TaskName = "Upload new ASP.NET video on YouTube",
+                    Completed = false,
+                    CategoryId = 2
+            },
+            new()
+            {
+                    TaskName = "Fix Settings.cs class of the project",
+                    Completed = false,
+                    CategoryId = 2
+            },
+            new()
+            {
+                    TaskName = "Update github repository",
+                    Completed = true,
+                    CategoryId = 2
+            },
+            new()
+            {
+                    TaskName = "Buy eggs",
+                    Completed = false,
+                    CategoryId = 3
+            },
+            new()
+            {
+                    TaskName = "Go for the pepperoni pizza",
+                    Completed = false,
+                    CategoryId = 3
+            }];
+
+        UpdateData();
+    }
+
+    public void UpdateData()
+    {
+        Categories.ToList().ForEach(c =>
+        {
+            int totalTasks = Tasks.Count(t => t.CategoryId == c.Id);
+            int completedTasks = Tasks.Count(t => t.CategoryId == c.Id && t.Completed);
+            c.PendingTasks = totalTasks - completedTasks;
+            c.Percentage = (float)completedTasks / totalTasks * 100;
+            Tasks.Where(t => t.CategoryId == c.Id).ToList().ForEach(t =>
+            {
+                t.TaskColor = c.Color;
+            });
+        });
+    }
 }
