949056e39160bbac5f12a0905aeb1e2a7a001ba9
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-07-31 20:43:31 +0100

    Creating the categories section 1

--- Tasker/ViewModels/MainPageViewModel.cs
+++ Tasker/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace Tasker.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Tasker.Models;
+
+namespace Tasker.ViewModels;
 
 public class MainPageViewModel
 {
+    public ObservableCollection<Category> Categories { get; set; } = [];
 
+    public MainPageViewModel()
+    {
+        FillData();
+    }
+
+    private void FillData() => Categories = [
+        new()
+        {
+                Id = 1,
+                CategoryName = ".NET MAUI Course",
+                Color = "#CF14DF"
+        },
+        new()
+        {
+                Id = 2,
+                CategoryName = "Tutorials",
+                Color = "#df6f14"
+        },
+        new()
+        {
+                Id = 3,
+                CategoryName = "Shopping",
+                Color = "#14df80"
+        }];
 }
